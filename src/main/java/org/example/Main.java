5165076410944b0b83b79ff87641da4973c4cbae
MarSerMer <marinakms@yandex.ru>
2023-02-16 12:58:05 +0300

    Homework 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
         store.addProduct(new Products("Chips", 72.99))
                 .addProduct(new Products("Coke",59.49))
                 .addProduct(new Products("Bread",52.29))
-                .addProduct(new Products("Juice",55.20));
+                .addProduct(new Products("Juice",55.25));
         store.addProduct(new PerishableProducts("Milk", 62.99, 5));
 
 
@@ -18,34 +18,36 @@ public class Main {
         System.out.println(store.findProduct("Chips"));*/
 
         store.buy("Coke", 59.49);
-        System.out.println("You bought Coke");
         System.out.println(store);
-        System.out.println("---------");
         //System.out.println(store.getMoney()); в новом оверрайде тустринга и так будет выводиться
 
         store.buy("Bread",52.29);
-        System.out.println("You bought Bread");
         System.out.println(store);
-        System.out.println("---------");
 
-
-        store.buy("Juice",55.20);
-        System.out.println("You bought Juice");
+        store.buy("Juice",55.25);
         System.out.println(store);
-        System.out.println("---------");
 
         store.addProduct(new Beer("Beer0", 60.00));
         store.addProduct(new Beer("Beer", 75.00, 12));
         System.out.println(store);
 
         store.buy("Milk", 62.99);
-        System.out.println("You bought Milk");
         System.out.println(store);
-        System.out.println("---------");
+
+        store.addProduct(new Water("WaterSp", 29.00, "sparkling"));
+        store.addProduct(new Water("WaterSt", 29.00, "still"));
+        System.out.println(store);
+
+        store.buy("Beer0", 60.00);
+        System.out.println(store);
+
+        store.addProduct(new ForAllergicPeople("Bar for allergy sufferers", 20.00, "nuts"));
+        store.addProduct(new ForAllergicPeople("Bar for allergy sufferers", 20.00, "gluten"));
+        store.addProduct(new ForAllergicPeople("Bar for allergy sufferers", 20.00, "eggs"));
+        System.out.println(store);
+
+        store.buy("Bar for allergy sufferers", 20.00);
+        System.out.println(store);
+
     }
-
-
-
-
-
 }
--- src/main/java/org/example/VendingMachine.java
+++ src/main/java/org/example/VendingMachine.java
@@ -32,10 +32,11 @@ public class VendingMachine {
     public Products buy(String name, Double price){
         Products something = findProduct(name);
         if(something==null) return null;
+        System.out.println("You are buying " + name);
         if (price.equals(something.getPrice())){
             products.remove(something);
             money +=price;
-            System.out.println("You spent:" + money);
+            //System.out.println("You spent:" + money);
             return something;
         }
         System.out.println("Wrong price. Please take your money back.");
@@ -52,11 +53,14 @@ public class VendingMachine {
     @Override
     public String toString() {
         StringBuilder wdProducts = new StringBuilder();
+        wdProducts.append("List of all products left in vending machine:\n");
         for (Products a : this.getProducts()) {
             wdProducts.append(a.toString());
             wdProducts.append("\n");
         }
+        wdProducts.append("Sum of money for all bought products: ");
         wdProducts.append(money.toString());
+        wdProducts.append("\n");
         return wdProducts.toString();
     }
 
